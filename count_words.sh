9543ce3cd0e9de8e56063d50da5bb353eb268bb2
fuxiaoming <fuxiaoming@kingsoft.com>
2023-07-13 17:00:44 +0800

    feat(script): 添加单词计数脚本
    添加了一个 Bash 脚本，用于统计指定文本文件中的行数和单词数。

---
+++ count_words.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 检查命令行参数是否为空
+if [ -z "$1" ]; then
+  echo "Usage: $0 file"
+  exit 1
+fi
+
+# 统计行数和单词数
+line_count=$(wc -l < "$1")
+word_count=$(wc -w < "$1")
+
+# 输出统计结果
+echo "Number of lines: $line_count"
+echo "Number of words: $word_count"
